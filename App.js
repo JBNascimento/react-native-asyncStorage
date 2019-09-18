ebf51480fa4c6e21cb29dab4cb6cf55da4c4f3cd
JefBNascimento <jbnascimento@msn.com>
2019-09-18 17:57:48 -0300

    Fiat lux

---
+++ App.js
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+}from 'react-native';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+   
+
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+});
+
+
