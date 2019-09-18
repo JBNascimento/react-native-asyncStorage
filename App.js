a2b86d81cf142f5c435f00a4a95deb049d9810d7
JefBNascimento <jbnascimento@msn.com>
2019-09-18 18:07:12 -0300

    Save and show a simple user

--- App.js
+++ App.js
@@ -5,16 +5,39 @@ import {
   StyleSheet,
   View,
   Text,
+  TouchableOpacity,
+  AsyncStorage
 }from 'react-native';
 
 export default class App extends React.Component{
   render(){
     return(
       <View style={styles.container}>
+        <TouchableOpacity onPress={this.saveData}>
+          <Text>Click me to save data</Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity onPress={this.displayData}>
+          <Text>Click me to dusplay data</Text>
+        </TouchableOpacity>
       </View>
     );
   }
+  saveData(){
+    let user = 'John Doe';
+    AsyncStorage.setItem('user', user);
+  }
+
+  displayData = async () => {
+    try {
+       let user = await AsyncStorage.getItem('user');
+       alert(user);
+
+    } catch (error) {
+      alert(error);
+    }
+  }
+  
 }
 
    
